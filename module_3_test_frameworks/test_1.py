e938ccdd6831e670d22e0fa23fff748939633b10
Andrew <gish.gish@inbox.ru>
2022-05-25 12:27:00 +0300

    added first task from module 3

---
+++ module_3_test_frameworks/test_1.py
@@ -0,0 +1,45 @@
+import unittest
+import time
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+def get_result(link):
+    with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as browser:
+        browser.get(link)
+        # name
+        f_name = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your first name"]')
+        f_name.send_keys("Name")
+        # surname
+        surname = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your last name"]')
+        surname.send_keys("Surname")
+        # e-mail
+        mail = browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Input your email"]')
+        mail.send_keys("my@mail.ru")
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+        # Проверяем, что смогли зарегистрироваться ждем загрузки страницы
+        time.sleep(1)
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # возвращаем текст элемента
+        return welcome_text_elt.text
+
+
+class TestAbs(unittest.TestCase):
+
+    def test_abs1(self):
+        res = get_result("http://suninjuly.github.io/registration1.html")
+        self.assertEqual("Congratulations! You have successfully registered!", res)
+
+    def test_abs2(self):
+        res = get_result("http://suninjuly.github.io/registration2.html")
+        self.assertEqual("Congratulations! You have successfully registered!", res)
+
+
+if __name__ == "__main__":
+    unittest.main()
