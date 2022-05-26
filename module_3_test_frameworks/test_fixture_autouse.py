928b92a204c63b619496fabb7e4b994aaf5e67ed
Andrew <gish.gish@inbox.ru>
2022-05-26 11:51:13 +0300

    autouse in fixtures

---
+++ module_3_test_frameworks/test_fixture_autouse.py
@@ -0,0 +1,37 @@
+"""
+https://stepik.org/lesson/237257/step/6?unit=209645
+"""
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
+
+
+class TestMainPage1:
+    # вызываем фикстуру в тесте, передав ее как параметр
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
