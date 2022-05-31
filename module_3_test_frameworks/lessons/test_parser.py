e868012a9cfc66281f7ec37d41c73e2c82c435a1
Andrew <gish.gish@inbox.ru>
2022-05-31 16:18:03 +0300

    params to command line

--- module_3_test_frameworks/lessons/test_parser.py
+++ module_3_test_frameworks/lessons/test_parser.py
@@ -6,3 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+#  pytest -s -v --browser_name=firefox test_parser.py
