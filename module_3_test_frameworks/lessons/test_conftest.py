ca9cbe7dca2a7d9256da364b6f45fcd7c22897bb
Andrew <gish.gish@inbox.ru>
2022-05-31 12:46:39 +0300

    test with conftest.py

---
+++ module_3_test_frameworks/lessons/test_conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+if __name__ == '__main__':
+    pytest.main()
