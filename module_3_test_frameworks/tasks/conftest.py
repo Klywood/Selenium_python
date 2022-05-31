43a496b60d835c48be86f5dbbc596e42dbeb8554
Andrew <gish.gish@inbox.ru>
2022-05-31 16:53:28 +0300

    set different languages task

--- module_3_test_frameworks/tasks/conftest.py
+++ module_3_test_frameworks/tasks/conftest.py
@@ -1,20 +1,42 @@
 import pytest
-from selenium import webdriver
-
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
+import os
 import logging
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 #  disable webdriver-manager logs
 logging.getLogger('WDM').setLevel(logging.NOTSET)
 res = ''
 
 
-@pytest.fixture(scope="class")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language: ru, en, fr, etc.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
