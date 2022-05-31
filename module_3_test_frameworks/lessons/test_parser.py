95d1477003aa2f7226466d56c191c072f1e4e858
Andrew <gish.gish@inbox.ru>
2022-05-31 16:22:51 +0300

    test reruns

--- module_3_test_frameworks/lessons/test_parser.py
+++ module_3_test_frameworks/lessons/test_parser.py
@@ -1,3 +1,9 @@
+
+"""
+https://stepik.org/lesson/237240/step/6?unit=209628
+https://stepik.org/lesson/237240/step/7?unit=209628
+"""
+
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -7,4 +13,10 @@ def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
 
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#magic_link")
+
 #  pytest -s -v --browser_name=firefox test_parser.py
+'''With reruns'''
+# pytest -v --tb=line --reruns 1 --browser_name=chrome test_parser.py
