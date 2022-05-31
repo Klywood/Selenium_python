41279cc0e62c6242ffe4408276aab73149a4ec3b
Andrew <gish.gish@inbox.ru>
2022-05-31 12:55:22 +0300

    params to command line

--- module_3_test_frameworks/lessons/conftest.py
+++ module_3_test_frameworks/lessons/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 
+from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
@@ -11,10 +12,24 @@ logging.getLogger('WDM').setLevel(logging.NOTSET)
 res = ''
 
 
-@pytest.fixture(scope="class")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
+
